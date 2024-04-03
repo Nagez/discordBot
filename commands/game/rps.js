fce8cf8d01ba84f8ed58d4573a11b070e4216f30
Nagez <81879748+Nagez@users.noreply.github.com>
2024-04-03 20:08:36 +0300

    added lizard spock version to rps

--- commands/game/rps.js
+++ commands/game/rps.js
@@ -1,12 +1,18 @@
 //rock paper scissor multiplayer game (with no slash command builder) https://www.youtube.com/watch?v=bDEgBZmsdI4
 //https://emojidb.org/
-//https://static.wikia.nocookie.net/bigbangtheory/images/7/7d/RPSLS.png/revision/latest?cb=20120822205915c
 const { ChatInputCommandInteraction, ApplicationCommandOptionType, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, SlashCommandBuilder } = require('discord.js');
 
-const choices = [
-    { name: 'Rock', emoji: '🪨', beats: 'Scissors' },
-    { name: 'Paper', emoji: '📄', beats: 'Rock' },
-    { name: 'Scissors', emoji: '✂️', beats: 'Paper' },
+const choices_regular = [
+    { name: 'Rock', emoji: '🪨', beats: ['Scissors'] },
+    { name: 'Paper', emoji: '📄', beats: ['Rock'] },
+    { name: 'Scissors', emoji: '✂️', beats: ['Paper'] },
+];
+const choices_LS = [
+    { name: 'Rock', emoji: '🪨', beats: ['Scissors','Lizard'] },
+    { name: 'Paper', emoji: '📄', beats: ['Rock','Spock'] },
+    { name: 'Scissors', emoji: '✂️', beats: ['Paper','Lizard'] },
+    { name: 'Lizard', emoji: '🦎', beats: ['Paper','Spock'] },
+    { name: 'Spock', emoji: '🖖', beats: ['Scissors','Rock'] },
 ];
 
 TIMEOUT = 2_400_000;
@@ -14,25 +20,48 @@ TIMEOUT = 2_400_000;
 module.exports = {
     category: 'game',
     data: new SlashCommandBuilder()
-        .setName('rps')
-        .setDescription('rock paper scissors game.')
+        .setName('rock_paper_scissors')
+        .setDescription('Rock Paper Scissors game.')
         .setDMPermission(false) //prevent being used in a direct message
-        .addUserOption(option =>
-            option.setName('user')
-                .setDescription('The user to play with')
-                .setRequired(true)
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('lizard_spock')
+                .setDescription('rock paper scissors lizard spock edition.')
+                .addUserOption(option =>
+                    option.setName('user')
+                        .setDescription('The user to play with')
+                        .setRequired(true))                
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('regular')
+                .setDescription('rock paper scissors game.')
+                .addUserOption(option =>
+                    option.setName('user')
+                        .setDescription('The user to play with')
+                        .setRequired(true))
         ),
+
     /**
     * @param {ChatInputCommandInteraction} interaction
     */
-    async execute( interaction ) {
+    async execute(interaction) {
 
         try {
             const targetUser = interaction.options.getUser('user');
-
+            
             if (interaction.user.id === targetUser.id || targetUser.bot) {
                 interaction.reply({ content: 'you can not play rock paper scissors with yourself or a bot.', ephemeral: true, });
                 return;
+            }
+            
+            const subcommand = interaction.options.getSubcommand();
+
+            let choices = choices_regular;
+            let image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Rock-paper-scissors.svg/1200px-Rock-paper-scissors.svg.png';
+            if (subcommand === 'lizard_spock') {
+                choices = choices_LS;
+                image = 'https://static.wikia.nocookie.net/bigbangtheory/images/7/7d/RPSLS.png/revision/latest?cb=20120822205915c'
             }
 
             const embed = new EmbedBuilder()
@@ -40,6 +69,7 @@ module.exports = {
                 .setDescription(`It's currently ${targetUser} turn`)
                 .setColor('Yellow')
                 .setTimestamp(new Date())
+                .setImage(image)
 
             const buttons = choices.map((choice) => {
                 return new ButtonBuilder()
@@ -59,17 +89,17 @@ module.exports = {
 
             const targetUserInteraction = await reply.awaitMessageComponent({
                 filter: (i) => i.user.id === targetUser.id,
-                time: TIMEOUT, 
-            }).catch(async (error) =>{
+                time: TIMEOUT,
+            }).catch(async (error) => {
                 embed.setDescription(`The time for a reply from ${targetUser} ran out.`);
-                await reply.edit({embeds: [embed], components: [] });
+                await reply.edit({ embeds: [embed], components: [] });
             })
             if (!targetUserInteraction) return;
 
-            const targetUserChoice = choices.find (
+            const targetUserChoice = choices.find(
                 (choice) => choice.name === targetUserInteraction.customId,
             );
-            
+
             await targetUserInteraction.reply({
                 content: `You picked ${targetUserChoice.name + targetUserChoice.emoji}`,
                 ephemeral: true,
@@ -85,37 +115,37 @@ module.exports = {
             const initialUserInteraction = await reply.awaitMessageComponent({
                 filter: (i) => i.user.id === interaction.user.id,
                 time: TIMEOUT, //30 seconds
-            }).catch(async (error) =>{
+            }).catch(async (error) => {
                 embed.setDescription(`The time for a reply from ${interaction.user} ran out.`);
-                await reply.edit({embeds: [embed], components: [] });
+                await reply.edit({ embeds: [embed], components: [] });
             })
             if (!initialUserInteraction) return;
 
-            const initialUserChoice = choices.find (
-                (choice) => choice.name === initialUserChoice.customId,
+            const initialUserChoice = choices.find(
+                (choice) => choice.name === initialUserInteraction.customId,
             );
 
             let result;
 
-            if(targetUserChoice.beats === initialUserChoice.name) {
+            if (targetUserChoice.beats.includes(initialUserChoice.name)) {
                 result = `${targetUser} won!`
             }
 
-            if(initialUserChoice.beats === targetUserChoice.name) {
+            if (initialUserChoice.beats.includes(targetUserChoice.name)) {
                 result = `${interaction.user} won!`
             }
 
-            if(initialUserChoice.name === targetUserChoice.name) {
+            if (initialUserChoice.name === targetUserChoice.name) {
                 result = `It was a tie!`
             }
 
             embed.setDescription(
                 `${targetUser} picked ${targetUserChoice.name + targetUserChoice.emoji}\n
                 ${interaction.user} picked ${initialUserChoice.name + initialUserChoice.emoji}\n
-                \n${result}`   
+                \n${result}`
             );
 
-            reply.edit({embeds: [embed], components: [] })
+            reply.edit({ embeds: [embed], components: [] })
 
         } catch (error) {
             console.log('Error in rps');
